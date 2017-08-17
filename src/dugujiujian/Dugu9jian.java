63e65b56a7feec47407e7c327d4274fac4bad894
令狐冲 <1071994522@qq.com>
2017-08-17 21:11:57 +0800

    修改

--- src/dugujiujian/Dugu9jian.java
+++ src/dugujiujian/Dugu9jian.java
@@ -4,6 +4,10 @@ public class Dugu9jian {
 
 	public static void main(String[] args) {
 		System.out.println("hello 123");
+		System.out.println("hello 456");
+		System.out.println("hello 456");
+		System.out.println("hello 456");
+		System.out.println("hello 456");
 	}
 
 }
