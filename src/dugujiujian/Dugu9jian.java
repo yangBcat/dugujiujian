5999c3766c7745fa90c3c7f66c27061f81e3128e
岳不群 <1847748975@qq.com>
2017-08-17 21:06:44 +0800

    修改

--- src/dugujiujian/Dugu9jian.java
+++ src/dugujiujian/Dugu9jian.java
@@ -4,6 +4,11 @@ public class Dugu9jian {
 
 	public static void main(String[] args) {
 		System.out.println("hello 123");
+		System.out.println("hello 123");
+		System.out.println("hello 123");
+		System.out.println("hello 123");
+		System.out.println("hello 123");
+		System.out.println("hello 123");
 	}
 
 }
